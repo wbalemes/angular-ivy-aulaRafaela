8fd9ecc29c2054aaefe94be0c3ee592aeda9c0ab
wbalemes <warleybruno54@hotmail.com>
2022-05-28 16:46:04 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-wjdyyy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-wjdyyy)
---
+++ src/app/lista-produtos/lista-produtos.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-produtos',
+  templateUrl: './lista-produtos.component.html',
+  styleUrls: ['./lista-produtos.component.css']
+})
+export class ListaProdutosComponent {
+
+}
